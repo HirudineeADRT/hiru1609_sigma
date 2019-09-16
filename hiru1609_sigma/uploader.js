afecbe09c0d352596732c0fd5a93f0cb662479ec
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-16 16:54:58 +0530

    added s3 bucket

--- hiru1609_sigma/uploader.js
+++ hiru1609_sigma/uploader.js
@@ -1,10 +1,30 @@
-exports.handler = function(event, context, callback) {
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
     console.log(event);
-    callback(null, {
+
+    s3.putObject({
+        "Body": image,
+        "Bucket": "hirudinee0508",
+        "Key": "test",
+        "ACL": "public-read"
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           
+                callback(null, {
         "isBase64Encoded": true,
         "statusCode": 200,
         "headers": {
-                "headerName": "headerValue"
-            },
-        "body": "..."});
+            "headerName": "headerValue"
+        },
+        "body": "..."
+    });
+        })
+        .catch(err => {
+            console.log(err, err.stack);
+            callback(err); // an error occurred
+        });
+
 }
